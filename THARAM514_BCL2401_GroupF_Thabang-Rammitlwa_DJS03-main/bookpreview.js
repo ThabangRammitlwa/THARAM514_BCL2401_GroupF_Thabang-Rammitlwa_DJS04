ed99d35e71b760dcdb20afc80fae7fb610f4c52c
Thabang Rammitlwa <thabang451@gmail.com>
2024-05-17 16:15:10 +0200

    updated

--- THARAM514_BCL2401_GroupF_Thabang-Rammitlwa_DJS03-main/bookpreview.js
+++ THARAM514_BCL2401_GroupF_Thabang-Rammitlwa_DJS03-main/bookpreview.js
@@ -1,58 +1,54 @@
-import { authors } from './data.js';
 class BookPreview extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `
-    <link rel="stylesheet" href="book-preview-styles.css">
-      
-        <div class="overlay">
-          <div class="overlay__preview">
-            <img class="overlay__blur" data-list-blur src="" />
-            <img class="overlay__image" data-list-image src="" />
-          </div>
-          <div class="overlay__content">
-            <h3 class="overlay__title" data-list-title></h3>
-            <div class="overlay__data" data-list-subtitle></div>
-            <p class="overlay__data overlay__data_secondary" data-list-description></p>
-          </div>
-          <div class="overlay__row">
-            <button class="overlay__button overlay__button_primary" data-list-close>Close</button>
-          </div>
-        </div>
-    <dialog class="overlay" data-list-active>
-    <div class="overlay__preview"><img class="overlay__blur" data-list-blur src=""/><img class="overlay__image" data-list-image src=""/></div>
-    <div class="overlay__content">
-      <h3 class="overlay__title" data-list-title></h3>
-      <div class="overlay__data" data-list-subtitle></div>
-      <p class="overlay__data overlay__data_secondary" data-list-description></p>
-    </div>
-    <div class="overlay__row">
-      <button class="overlay__button overlay__button_primary" data-list-close>Close</button>
-    </div>
-  </dialog>
+    constructor() {
+      super();
+  
+      // Create shadow DOM and append template content
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.innerHTML=`
+ <><div class="overlay">
+              <div class="overlay__preview">
+                  <img class="overlay__blur" data-list-blur src="" />
+                  <img class="overlay__image" data-list-image src="" />
+              </div>
+              <div class="overlay__content">
+                  <h3 class="overlay__title" data-list-title></h3>
+                  <div class="overlay__data" data-list-subtitle></div>
+                  <p class="overlay__data overlay__data_secondary" data-list-description></p>
+              </div>
+              <div class="overlay__row">
+                  <button class="overlay__button overlay__button_primary" data-list-close>Close</button>
+              </div>
+          </div><dialog class="overlay" data-list-active>
+                  <div class="overlay__preview"><img class="overlay__blur" data-list-blur src="" /><img class="overlay__image" data-list-image src="" /></div>
+                  <div class="overlay__content">
+                      <h3 class="overlay__title" data-list-title></h3>
+                      <div class="overlay__data" data-list-subtitle></div>
+                      <p class="overlay__data overlay__data_secondary" data-list-description></p>
+                  </div>
+                  <div class="overlay__row">
+                      <button class="overlay__button overlay__button_primary" data-list-close>Close</button>
+                  </div>
+              </dialog></>
       
       `;
-  }
+    }
+    
+  
     connectedCallback() {
-    this.shadowRoot
-      .querySelector('[data-list-close]')
-      .addEventListener('click', () => {
-        this.dispatchEvent(new CustomEvent('close'));
+      // Set attributes and text content based on provided data
+      const { id, image, title, author } = this.dataset;
+      this.shadowRoot.querySelector('.preview').setAttribute('data-preview', id);
+      this.shadowRoot.querySelector('.preview__image').src = image;
+      this.shadowRoot.querySelector('.preview__title').textContent = title;
+      this.shadowRoot.querySelector('.preview__author').textContent = author;
+      
+      // Add event listener for clicking on the preview
+      this.shadowRoot.querySelector('.preview').addEventListener('click', () => {
+        this.dispatchEvent(new CustomEvent('preview-click', { detail: id }));
       });
+    }
+  
   }
-  /**
-   * @param {{ author: any; description: any; image: any; published: any; title: any; }} book
-   */
-  set book(book) {
-    const { author, description, image, published, title } = book;
-    this.shadowRoot.querySelector('[data-list-blur]').src = image;
-    this.shadowRoot.querySelector('[data-list-image]').src = image;
-    this.shadowRoot.querySelector('[data-list-title]').innerText = title;
-    this.shadowRoot.querySelector('[data-list-subtitle]').innerText =
-      `${authors[author]} (${new Date(published).getFullYear()})`;
-    this.shadowRoot.querySelector('[data-list-description]').innerText =
-      description;
-  }
-}
-customElements.define('book-preview', BookPreview);
+  // Define the custom element
+  customElements.define('book-preview', BookPreview);
+  
